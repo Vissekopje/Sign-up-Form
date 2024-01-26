a0e2ad27d610c936be0c94fbafc4a7a2ac7e2767
Vissekopje <78539135+Vissekopje@users.noreply.github.com>
2024-01-26 12:30:45 +0100

    Finished up styling and added password match

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
-let password = document.getElementById("password")
-let confirmpassword = document.getElementById("Confirm-password")   
+let pwInput = document.querySelector('.form-row input#password');
+let pwConfirmInput = document.querySelector('.form-row input#confirmpassword');
+const sumbitButton = document.querySelector(".submit")
 
 let confirmPasswordError = document.querySelector(".Confirm-password-error")
-function applejuice() {
-    if (password !== confirmpassword){
-        e.preventDefault()
-        confirmPasswordError.textcontent = "Passwords do not match";
+
+
+function validatePassword() {
+    if (pwInput.value !== pwConfirmInput.value) {
+        pwConfirmInput.setCustomValidity("Passwords do not match");
+        confirmPasswordError.textContent = "Do not match"
         console.log("Error")
     }
+    else {
+        confirmPasswordError.textContent = ""
+    }
 }
+
+
+pwConfirmInput.addEventListener("input", validatePassword);
 
